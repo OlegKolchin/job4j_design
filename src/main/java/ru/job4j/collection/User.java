200d46524947d8a442ea19d6b8568e12541bdf59
OlegKolchin <commshepard@gmail.com>
2021-03-11 06:37:15 +0500

    1. Создать модель User [#409866]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,8 +1,18 @@
 package ru.job4j.collection;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
     public String getName() {
         return name;
     }
@@ -27,7 +37,16 @@ public class User {
         this.birthday = birthday;
     }
 
-    private String name;
-    private int children;
-    private Calendar birthday;
+
+    public static void main(String[] args) {
+        User user1 = new User("Vasiliy", 1, Calendar.getInstance());
+        User user2 = new User("Vasiliy", 1, Calendar.getInstance());
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        for (User key : map.keySet()) {
+           Object value = map.get(key);
+           System.out.println(key + " " + value);
+        }
+    }
 }
